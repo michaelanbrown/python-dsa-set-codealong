cc87726448f50be34c0268ad0890d9ad6a417f1d
michaelanbrown <m_brownbrown@yahoo.com>
2024-11-19 15:36:31 -0500

    Added empty dict storing keys

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,5 @@
 class MySet
-  def __init__(self, enumerable = []):
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
